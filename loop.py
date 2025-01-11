e1adc96ff3fbc2934e83040c09e325f86833cd6d
pache-05 <hassanhmdt@gmail.com>
2025-01-11 13:54:26 +0100

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,6 @@
+x = 10
+y = 1
+
+while y < x:
+    print("welcome")
+    y += 1 
---
+++ practice.py
@@ -0,0 +1,39 @@
+#name =["hassan", "abubakar", "phillips","hassan","hassan"]
+#print(dir(name))
+#APPEND
+
+
+# name.append("Emmanuel")
+# name.append(True)
+
+#EXTEND AND INSERT
+# new_names =["vicent", "james", ]
+# name.extend(new_names)
+# name.insert(0,"Mohammed")
+# name.clear()
+# another_name=name.copy()
+# del(name)
+
+#POP
+# pname ="hassan"
+# count_result= name.count(pname)
+#poped =name.pop(2)
+#name.remove("hassan")
+#print(poped)
+
+# cars =[["m5","m6"], "audi","toyota"]
+# cars[0][1] ="m1"
+# print(cars)
+
+# DICTIONARY
+cars ={"bmw":"m5","audi":"audi", "number":4, "status":True}
+# cars["status"]= False
+
+# our_carkeys = cars.keys()
+
+our_values =list(cars.values())
+
+print(our_values)
+
+#print(dir(cars))
+
